b2668b426efd41cb79cb06b2bf7e52e1a9cc2e56
Med Aduh <im.ramidem@gmail.com>
2023-02-19 23:50:19 +0800

    hello world

---
+++ index.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+const HOSTNAME = "127.0.0.1";
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.end(
+    JSON.stringify({
+      message: "Hello, World@",
+    })
+  );
+});
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log(`Server running at http://${HOSTNAME}:${PORT}`);
+});
